7aca85b6d8ad233b28bf5aa422d1aa89399e36f9
moeinTavakoli <mtawakoli2283@gmail.com>
2023-12-17 18:34:31 +0330

    [ Fix ] fix typo

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ const config = {
         user: process.env.DATABASE_USER ,
         password: process.env.DATABASE_PASSWORD ,
         database : process.env.DATABASE_NAME , 
-        authPlugin: 'mysql_nativ`e_password' 
+        authPlugin: 'mysql_native_password' 
 
     } , 
     server : 
