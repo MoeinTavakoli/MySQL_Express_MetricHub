46d8336f0653bbe1f64c018785c89da363d3d791
moeinTavakoli <mtawakoli2283@gmail.com>
2023-12-17 18:31:25 +0330

    [ Init ] initialize

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+// use morgan lib
+const morgan = require('morgan')
+app.use(morgan('tiny'))
+
+// import server config
+const {config} = require('./config')
+const port = config.server.port
+
+
+require('./db/_connection.js')
+require('./db/customers.js')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port , () => {
+  console.log(`Example app listening on port ${port}`)
+})
