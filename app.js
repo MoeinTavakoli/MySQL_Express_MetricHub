9fca991d052dd453a46ae396df0ae784e5525e6d
moeinTavakoli <mtawakoli2283@gmail.com>
2023-12-18 12:20:18 +0330

    [ Improvment ] clean up - add server loader

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
-const express = require('express')
-const app = express()
+const app = require('./loader/server.js')
 
-// use morgan lib
-const morgan = require('morgan')
-app.use(morgan('tiny'))
 
 // import server config
 const config = require('./config')
 const port = config.server.port
 
-
-require('./db/_connection.js')
-require('./db/customers.js')
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-// add prometheus register 
-const register = require('./service/prometheus.js')
-
-// route /metrics 
-app.get('/metrics', async(req, res) => {
-  const metrics = await  register.metrics()
-  res.set('Content-Type', register.contentType)
-  res.end(metrics)
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
